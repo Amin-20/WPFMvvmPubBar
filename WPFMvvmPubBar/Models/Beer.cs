197b66d6a9efe173255fcef226f31f2a64213f5e
Ataki_pw11 <Ataki_pw11@STHQ0127-05>
2023-03-09 13:51:51 +0400

    Images done

---
+++ WPFMvvmPubBar/Models/Beer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMvvmPubBar.Models
+{
+    public class Beer : Entity
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Volume { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
---
+++ WPFMvvmPubBar/Repositories/FakeRepo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMvvmPubBar.Models;
+
+namespace WPFMvvmPubBar.Repositories
+{
+    public class FakeRepo
+    {
+
+        public List<Beer> GetAll()
+        {
+            return new List<Beer>
+            {
+                new Beer
+                {
+                    Name="Xirdalan",
+                    Price=2.20,
+                    Volume=4.8,
+                    ImagePath="/Images/xirdalan.png"
+                },
+                new Beer
+                {
+                    Name="NZS",
+                    Price=1.95,
+                    Volume=10,
+                    ImagePath="/Images/Nzs.jpg"
+                },
+                new Beer
+                {
+                    
+                }
+
+            };
+        }
+    }
+}
